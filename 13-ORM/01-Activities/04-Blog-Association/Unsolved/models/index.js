ea9d31a179059f71a573715d750ca19693fa6fda
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-06 20:17:04 -0600

    update

--- 13-ORM/01-Activities/04-Blog-Association/Unsolved/models/index.js
+++
@@ -1,8 +0,0 @@
-const Post = require('./post');
-const Author = require('./author');
-
-// Create a relationship that instructs Posts to belong to Authors
-Post.belongsTo(Author);
-// Create a relationship that instructs Authors that it has many Posts
-Author.hasMany(Post);
-module.exports = { Post, Author };
