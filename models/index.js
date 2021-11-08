d19bc2c9025a66b5a26cc2655c2835a5a3b2db3d
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-08 12:10:10 -0700

    add to category routes

--- models/index.js
+++ models/index.js
@@ -12,6 +12,32 @@ const ProductTag = require('./ProductTag');
 
 // Tags belongToMany Products (through ProductTag)
 
+// Products belongsTo Category
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
+// Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
 module.exports = {
   Product,
   Category,
