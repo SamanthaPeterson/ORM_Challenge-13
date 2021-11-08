e9280d5856f66e82cc5aa96011af73dc94057d3c
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-08 12:04:33 -0700

    add to models

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,50 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
+    // id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   primaryKey: true,
+    //   autoIncrement: true
+    // },
+    // comment_text: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   validate: {
+    //     len: [1]
+    //   }
+    // },
+    // user_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: 'user',
+    //     key: 'id'
+    //   }
+    // },
+    // post_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: 'post',
+    //     key: 'id'
+    //   }
+    // }
   },
   {
     sequelize,
