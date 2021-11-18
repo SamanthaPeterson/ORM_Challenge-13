95b601fbcecf025f961907d189aa009fa899a0da
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-17 21:36:56 -0700

    final push - help from instructor to fix put

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,7 +114,7 @@ router.put('/:id', (req, res) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
+      const newProductTags = req.body?.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
           return {
@@ -135,7 +135,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
